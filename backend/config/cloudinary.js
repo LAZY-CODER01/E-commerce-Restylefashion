a7bdc88ef7540220ca66b87dcb8a8dd1ee94d04a
LAZY-CODER01 <avinashmaurya430@gmail.com>
2026-03-29 18:40:49 +0530

    Fix(Cloudinary): Error resolved

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 require("dotenv").config(); // Force load env variables to prevent missing credential
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Configure Multer to use memory storage (compatible with Vercel serverless functions)
